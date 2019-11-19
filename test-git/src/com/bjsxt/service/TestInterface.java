9ab5026c59ef7058c83b52c6039080ebc0cbea10
jiayawen-git <jyw_nice@163.com>
2019-11-19 15:24:52 +0800

    测试

---
+++ test-git/src/com/bjsxt/service/TestInterface.java
@@ -0,0 +1,11 @@
+package com.bjsxt.service;
+/** 
+ * @Title TestInterface.java 
+ * @description: 
+ * @time 2019年11月19日 下午3:18:32 
+ * @author 贾亚文 
+ * @version 
+ **/
+public interface TestInterface {
+	void a();
+}
